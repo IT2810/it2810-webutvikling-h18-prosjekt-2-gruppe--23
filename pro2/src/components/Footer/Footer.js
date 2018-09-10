f35dc91a80a07c16aa070bd4c0e8a42ce899358a
JulieAdele <juliesjaavaag@outlook.com>
2018-09-10 09:47:15 +0200

    added footer.css

--- pro2/src/components/Footer/Footer.js
+++ pro2/src/components/Footer/Footer.js
@@ -3,13 +3,10 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Footer">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
