44f2272ddc84cf8bc45bc435e073e2fe56e6d362
JulieAdele <juliesjaavaag@outlook.com>
2018-09-13 08:54:17 +0200

    Flyttet mappen med lyder til Public, blir mer ryddig sånn

--- pro2/src/components/Sound/Sound.js
+++ pro2/src/components/Sound/Sound.js
@@ -1,18 +1,8 @@
 import React, {Component} from 'react';
-import mp3_animal0 from './LyderP2/1elephant.mp3';
-import mp3_animal1 from './LyderP2/1monkey.mp3';
-import mp3_animal2 from './LyderP2/1rooster.mp3';
-import mp3_animal3 from './LyderP2/1seagulls.mp3';
-import mp3_instrument0 from './LyderP2/2flute.mp3';
-import mp3_instrument1 from './LyderP2/2guitar.mp3';
-import mp3_instrument2 from './LyderP2/2harmonica.mp3';
-import mp3_instrument3 from './LyderP2/2trumpet.mp3';
-import mp3_feeling0 from './LyderP2/3chill.mp3';
-import mp3_feeling1 from './LyderP2/3happy.mp3';
-import mp3_feeling2 from './LyderP2/3sad.mp3';
-import mp3_feeling3 from './LyderP2/3spooky.mp3';
-//Har ikke tenkt å importere alt på en gang, gjør det bare foreløpig.
 
+
+let url = "Lyder/Instrument/4.mp3";
+// let url = "Lyder/"+category+"/"+tabnumber+".mp3";
 
 class Sound extends Component{
 
@@ -20,7 +10,7 @@ class Sound extends Component{
     return (
       <div className="soundContainer">
         <audio controls>
-          <source type="audio/mp3" src={mp3_animal1} />
+          <source type="audio/mp3" src={url} />
           Your browser does not support the audio element.
         </audio>
       </div>
